88d8f77f39e9fb49a78cdd539a4acc820e39f7ec
Justin Jones <jj@kickstarter.com>
2015-11-11 18:56:14 -0500

    permit message

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -69,6 +69,6 @@ class VisitsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def visit_params
-      params.require(:visit).permit(:name)
+      params.require(:visit).permit(:name, :message)
     end
 end
